5f849af96e9a72e8d2f89a960fca22582d3f6387
linxok <linxok@gmail.com>
2021-01-12 23:49:50 +0200

    magento2 lesson 23

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,18 +1,44 @@
-define(['uiComponent'], function (Component) {
+define([
+    'uiComponent',
+    'jquery'
+    ], function (Component, $) {
     'use strict';
     console.log("Hello World");
     return Component.extend({
         defaults: {
             tasks: [
-                {label: "Task1"},
-                {label: "Task2"},
-                {label: "Task3"},
-                {label: "Task4"}
+                {id: 1, label: "Task1", status: false},
+                {id: 2, label: "Task2", status: false},
+                {id: 3, label: "Task3", status: false},
+                {id: 4, label: "Task4", status: true}
                 ]
         },
         initObservable: function () {
             this._super().observe(['tasks']);
             return this;
-        }
+        },
+        switchStatus: function (data, event) {
+            const taskId = $(event.target).data('id');
+            let items = this.tasks().map(function (task) {
+               if (task.id === taskId){
+                   task.status = !task.status;
+               }
+               return task;
+            });
+
+            this.tasks(items);
+        },
+        deleteTask: function (taskId) {
+            if (this.tasks().length === 1){
+                return;
+            }
+            let tasks = [];
+            this.tasks().forEach(function (task) {
+                if (task.id !== taskId){
+                    tasks.push(task);
+                }
+            });
+            this.tasks(tasks);
+        },
     });
 });
