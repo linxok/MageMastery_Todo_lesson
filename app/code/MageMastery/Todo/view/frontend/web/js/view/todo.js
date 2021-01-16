ad4da9329810c0378887abfc5ad866d0e5f456e0
linxok <linxok@gmail.com>
2021-01-16 20:37:01 +0200

    magento2 lesson 27

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,44 +1,64 @@
 define([
     'uiComponent',
-    'jquery'
-    ], function (Component, $) {
+    'jquery',
+    'Magento_Ui/js/modal/confirm'
+], function (Component, $, modal) {
     'use strict';
     console.log("Hello World");
     return Component.extend({
         defaults: {
+            newTaskLabel: '',
             tasks: [
                 {id: 1, label: "Task1", status: false},
                 {id: 2, label: "Task2", status: false},
                 {id: 3, label: "Task3", status: false},
                 {id: 4, label: "Task4", status: true}
-                ]
+            ]
         },
         initObservable: function () {
-            this._super().observe(['tasks']);
+            this._super().observe(['tasks', 'newTaskLabel']);
             return this;
         },
         switchStatus: function (data, event) {
             const taskId = $(event.target).data('id');
             let items = this.tasks().map(function (task) {
-               if (task.id === taskId){
-                   task.status = !task.status;
-               }
-               return task;
+                if (task.id === taskId) {
+                    task.status = !task.status;
+                }
+                return task;
             });
 
             this.tasks(items);
         },
         deleteTask: function (taskId) {
-            if (this.tasks().length === 1){
-                return;
-            }
-            let tasks = [];
-            this.tasks().forEach(function (task) {
-                if (task.id !== taskId){
-                    tasks.push(task);
+            let self = this;
+            modal({
+                content: 'Are you sure you want to delete this?',
+                actions: {
+                    confirm: function () {
+
+                        let tasks = [];
+                        if (self.tasks().length === 1) {
+                            self.tasks(tasks);
+                            return;
+                        }
+                        self.tasks().forEach(function (task) {
+                            if (task.id !== taskId) {
+                                tasks.push(task);
+                            }
+                        });
+                        self.tasks(tasks);
+                    }
                 }
             });
-            this.tasks(tasks);
+        },
+        addTask: function () {
+            this.tasks.push( {
+                id: Math.floor(Math.random() * 100),
+                label: this.newTaskLabel(),
+                status: true
+            });
+            this.newTaskLabel('');
         },
     });
 });
